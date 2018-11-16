c79d25db86166993aa4684f0eea63562f013e776
Patrick Baus <patrick.baus@physik.tu-darmstadt.de>
2018-11-16 18:23:22 +0100

    - Updated coding style (tabs -> spaces) - Changed CborStaticOutput to accept a buffer instead of creating one itself. This allows the used of the same buffer for multiple decoders, like COBS - Changed the decoder - Added writeMap() function to write an indefinite size map - Added endMap(9 function to terminate an indefinite size map - Added onNull() listener event - Added onFloat()/onDouble listener event

--- CborDecoder.h
+++ CborDecoder.h
@@ -6,102 +6,71 @@
 #define INT_MAX 2
 #define INT_MIN 2
 
-
 typedef enum {
-	STATE_TYPE,
-	STATE_PINT,	
-    STATE_NINT,
-    STATE_BYTES_SIZE,
-    STATE_BYTES_DATA,
-    STATE_STRING_SIZE,
-    STATE_STRING_DATA,
-    STATE_ARRAY,
-    STATE_MAP,
-    STATE_TAG,
-    STATE_SPECIAL,
-    STATE_ERROR
+  STATE_TYPE,
+  STATE_PINT,
+  STATE_NINT,
+  STATE_BYTES_SIZE,
+  STATE_BYTES_DATA,
+  STATE_STRING_SIZE,
+  STATE_STRING_DATA,
+  STATE_ARRAY,
+  STATE_MAP,
+  STATE_TAG,
+  STATE_FLOAT,
+  STATE_ERROR
 } CborReaderState;
 
 class CborInput {
-	
-public:
-	CborInput(void *data, int size);
-	~CborInput();
 
-	bool hasBytes(unsigned int count);
-	unsigned char getByte();
-	unsigned short getShort();
-	uint32_t getInt();
-	uint64_t getLong();
-	void getBytes(void *to, int count);
-private:
-	unsigned char *data;
-	int size;
-	int offset;
+  public:
+    CborInput(void *data, int size);
+    ~CborInput();
+
+    bool hasBytes(unsigned int count);
+    unsigned char getByte();
+    unsigned short getShort();
+    uint32_t getInt();
+    uint64_t getLong();
+    void getBytes(void *to, int count);
+  private:
+    unsigned char *data;
+    int size;
+    size_t offset;
 };
-
-
-
 
 class CborListener {
 public:
-	virtual void OnInteger(int32_t value) = 0;
-	virtual void OnBytes(unsigned char *data, unsigned int size) = 0;
-	virtual void OnString(String &str) = 0;
-	virtual void OnArray(unsigned int size) = 0;
-	virtual void OnMap(unsigned int size) = 0;
-	virtual void OnTag(uint32_t tag) = 0;
-	virtual void OnSpecial(uint32_t code) = 0;
-	virtual void OnError(const char *error) = 0;
-    virtual void OnExtraInteger(uint64_t value, int sign) {}
-    virtual void OnExtraTag(uint64_t tag) {}
-    virtual void OnExtraSpecial(uint64_t tag) {}
-};
-
-class CborDebugListener : public CborListener {
-public:
-	virtual void OnInteger(int32_t value);
-	virtual void OnBytes(unsigned char *data, unsigned int size);
-	virtual void OnString(String &str);
-	virtual void OnArray(unsigned int size);
-	virtual void OnMap(unsigned int size);
-	virtual void OnTag(uint32_t tag);
-	virtual void OnSpecial(uint32_t code);
-	virtual void OnError(const char *error);
-
-    virtual void OnExtraInteger(uint64_t value, int sign);
-    virtual void OnExtraTag(uint64_t tag);
-    virtual void OnExtraSpecial(uint64_t tag);
+  virtual void OnInteger(int32_t value) = 0;
+  virtual void OnBoolean(const bool value) = 0;
+  virtual void OnBytes(unsigned char *data, unsigned int size) = 0;
+  virtual void OnString(String &str) = 0;
+  virtual void OnArray(unsigned int size) = 0;
+  virtual void OnMap(unsigned int size) = 0;
+  virtual void OnTag(uint32_t tag) = 0;
+  virtual void OnSpecial(uint32_t code) = 0;
+  virtual void OnError(const char *error) = 0;
+  virtual void onNull() = 0;
+  virtual void onFloat(const float value) = 0;
+  virtual void onDouble(const double value) = 0;
+  virtual void OnExtraInteger(uint64_t value, int8_t sign) {}
+  virtual void OnExtraTag(uint64_t tag) {}
+  virtual void OnExtraSpecial(uint64_t tag) {}
 };
 
 class CborReader {
-public:
-	CborReader(CborInput &input);
-	CborReader(CborInput &input, CborListener &listener);
-	~CborReader();
-	void Run();
-	void SetListener(CborListener &listener);
-	void GetCborData(String &Cborpackage);
-private:
-	CborListener *listener;
-	CborInput *input;
-	CborReaderState state;
-	unsigned int currentLength;
-};
-
-
-class CborExampleListener : public CborListener {
   public:
-    void OnInteger(int32_t value);
-    void OnBytes(unsigned char *data, unsigned int size);
-    void OnString(String &str);
-    void OnArray(unsigned int size);
-    void OnMap(unsigned int size) ;
-    void OnTag(uint32_t tag);
-    void OnSpecial(uint32_t code);
-    void OnError(const char *error);
-  };
-
-
-
+    CborReader(CborInput &input);
+    CborReader(CborInput &input, CborListener &listener);
+    ~CborReader();
+    void Run();
+    void SetListener(CborListener &listener);
+    void GetCborData(String &Cborpackage);
+  private:
+    CborListener *listener;
+    CborInput *input;
+    CborReaderState state;
+    unsigned int currentLength;
+};
 #endif
+
--- CborEncoder.h
+++ CborEncoder.h
@@ -1,17 +1,17 @@
 /*
-   Copyright 2014-2015 Stanislav Ovsyannikov
+  Copyright 2014-2015 Stanislav Ovsyannikov
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+  http://www.apache.org/licenses/LICENSE-2.0
 
-	   Unless required by applicable law or agreed to in writing, software
-	   distributed under the License is distributed on an "AS IS" BASIS,
-	   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	   See the License for the specific language governing permissions and
-	   limitations under the License.
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
 */
 
 
@@ -23,7 +23,7 @@
 #include "Arduino.h"
 
 class CborOutput {
-public:
+  public:
     virtual unsigned char *getData() = 0;
     virtual unsigned int getSize() = 0;
     virtual void putByte(unsigned char value) = 0;
@@ -31,32 +31,32 @@ public:
 };
 
 class CborStaticOutput : public CborOutput {
-public:
-	CborStaticOutput(unsigned int capacity);
-	~CborStaticOutput();
-	virtual unsigned char *getData();
-	virtual unsigned int getSize();
-	virtual void putByte(unsigned char value);
-	virtual void putBytes(const unsigned char *data, const unsigned int size);
-private:
-	unsigned char *buffer;
-	unsigned int capacity;
-	unsigned int offset;
+  public:
+    CborStaticOutput(unsigned char* _buffer, const unsigned int _size, const unsigned int _offset);
+    ~CborStaticOutput();
+    virtual unsigned char *getData();
+    virtual unsigned int getSize();
+    virtual void putByte(const unsigned char value);
+    virtual void putBytes(const unsigned char *data, const unsigned int size);
+    void resetBuffer();
+  private:
+    unsigned char *buffer;
+    unsigned int capacity;
+    unsigned int offset;
+    unsigned int cursor;
 };
 
-
 class CborDynamicOutput : public CborOutput {
-public:
+  public:
     CborDynamicOutput();
     CborDynamicOutput(uint32_t initalCapacity);
     ~CborDynamicOutput();
-
 
     virtual unsigned char *getData();
     virtual unsigned int getSize();
     virtual void putByte(unsigned char value);
     virtual void putBytes(const unsigned char *data, const unsigned int size);
-private:
+  private:
     void init(unsigned int initalCapacity);
     unsigned char *buffer;
     unsigned int capacity;
@@ -64,30 +64,35 @@ private:
 };
 
 class CborWriter {
-public:
-	CborWriter(CborOutput &output);
-	~CborWriter();
+  public:
+    CborWriter(CborOutput &output);
+    ~CborWriter();
 
-	void writeInt(const int value);
-	void writeInt(const int32_t value);
-	void writeInt(const int64_t value);
-	void writeInt(const uint32_t value);
-	void writeInt(const uint64_t value);
-	void writeBytes(const unsigned char *data, const unsigned int size);
-	void writeString(const char *data, const unsigned int size);
-	void writeString(const String str);
-	void writeArray(const unsigned int size);
-	void writeMap(const unsigned int size);
-	void writeTag(const uint32_t tag);
-	void writeSpecial(const uint32_t special);
-private:
-	void writeTypeAndValue(uint8_t majorType, const uint32_t value);
-	void writeTypeAndValue(uint8_t majorType, const uint64_t value);
-	CborOutput *output;
+    void writeInt(const int value);
+    void writeInt(const int32_t value);
+    void writeInt(const int64_t value);
+    void writeInt(const uint32_t value);
+    void writeInt(const uint64_t value);
+    void writeBytes(const unsigned char *data, const unsigned int size);
+    void writeString(const char *data, const unsigned int size);
+    void writeString(const String str);
+    void writeArray(const unsigned int size);
+    void writeMap(const unsigned int size);
+    void writeMap();
+    void endMap();
+    void writeTag(const uint32_t tag);
+    void writeSpecial(const uint32_t special);
+  private:
+    void writeBreakCode();
+    void writeTypeAndValue(uint8_t majorType, const uint32_t value);
+    void writeTypeAndValue(uint8_t majorType, const uint64_t value);
+    void writeTypeAndIndefiniteValue(const uint8_t majorType);
+    CborOutput *output;
 };
 
 class CborSerializable {
-public:
-	virtual void Serialize(CborWriter &writer) = 0;
+  public:
+    virtual void Serialize(CborWriter &writer) = 0;
 };
 #endif
+
